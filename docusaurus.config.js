2ce2f02a3617488e3d1f1e7e02d49d1b28b59d4a
Christine Belzie <105683440+CBID2@users.noreply.github.com>
2025-09-05 07:38:00 -0400

    docs (docusaurus.config.js): replace placeholder text with live site’s url (#11)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
     v4: true,
   },
 
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://refract-docs-green.vercel.app',
   baseUrl: '/',
 
   organizationName: 'facebook',
